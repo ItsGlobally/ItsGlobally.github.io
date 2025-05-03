35ff41aaf13c51cd4af26dbcab44d4ce7645aa6c
itsglobally1324 <itsglobally1324@gmail.com>
2025-05-03 04:54:04 +0000

    0

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,30 +35,19 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head />
-      <body
-        className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
+      <body className={clsx(
+          "min-h-screen bg-background font-sans antialiased bg-[url(/background.jpg)] ",
+          fontSans.variable
+        )} >
+        <Providers
+          themeProps={{ attribute: "class", defaultTheme: "dark" }}
+        >
+          <div className="relative flex flex-col h-screen backdrop-brightness-50">
+            <Navbar/>
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
-            </footer>
-          </div>
+            </div>
         </Providers>
       </body>
     </html>
